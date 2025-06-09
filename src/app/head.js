0002f04d4489c66bc023cd2ce454eed25f31336e
Murilo <murilinhonada@gmail.com>
2025-06-08 23:27:16 -0300

    Create head.js

---
+++ src/app/head.js
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Criando SaaS com IA</title>
+      <meta name="google-site-verification" content="_H1vSvWBdZ7qP5lOB0v35Ngljx9PN5nAWmUcCRCNcqY" />
+      {/* outras meta tags, se quiser */}
+    </>
+  )
+}
